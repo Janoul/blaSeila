cbe0b16d34b88f31c00720d471dae6fb6b8f8f1f
Edson Antonio da Silva Junior <edson.00000847957@unicap.br>
2023-12-13 21:05:54 -0300

    bora ver

--- src/main/java/com/BookThePhone/demo/service/ClienteService.java
+++ src/main/java/com/BookThePhone/demo/service/ClienteService.java
@@ -1,4 +1,42 @@
 package com.BookThePhone.demo.service;
 
+import com.BookThePhone.demo.model.Cliente;
+import com.BookThePhone.demo.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Autowired
+    public ClienteService(ClienteRepository clienteRepository){
+
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente criarCliente(Cliente cliente){
+
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarClientes(){
+
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> retornarClientePeloId(Long id){
+
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente retornarClientePeloNome(String nome){
+
+        return clienteRepository.findByNome(nome);
+    }
 }
--- src/main/java/com/BookThePhone/demo/service/ContaService.java
+++ src/main/java/com/BookThePhone/demo/service/ContaService.java
@@ -1,4 +1,40 @@
 package com.BookThePhone.demo.service;
 
-public class ContaService {
+import com.BookThePhone.demo.model.Cliente;
+import com.BookThePhone.demo.model.Conta;
+import com.BookThePhone.demo.repository.ClienteRepository;
+import com.BookThePhone.demo.repository.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContaService{
+
+    private final ContaRepository contaRepository;
+    private final ClienteRepository clienteRepository;
+
+    @Autowired
+    public ContaService(ContaRepository contaRepository, ClienteRepository clienteRepository) {
+        this.contaRepository = contaRepository;
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Conta> listarContas(){
+        return contaRepository.findAll();
+    }
+
+    public Conta criarConta(Conta conta, Long idCliente){
+        Optional<Cliente> optCliente = clienteRepository.findById(idCliente);
+
+        if (optCliente.isEmpty()){
+            return null;
+        } else {
+            optCliente.get().getContas().add(conta);
+            conta.setCliente(optCliente.get());
+            return contaRepository.save(conta);
+        }
+    }
 }
